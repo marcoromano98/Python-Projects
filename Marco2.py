3f4fc355a4255a0fa7647a880eae24dc80a92c8a
marcoromano98 <118452983+marcoromano98@users.noreply.github.com>
2022-12-03 20:24:54 +1000

    Add files via upload

---
+++ Marco2.py
@@ -0,0 +1,11 @@
+girlfriend_bio = {
+    #title of interest behind the double colon
+    #interests after the colon
+    "name": "Kenzie Albrecht",
+    "known for": ["Being the best girlfriend in the world"],
+    "favourite person": "Marco <3",
+    "hobby": "being with boyfriend",
+    "fav snack": "double coated tim tam"
+}
+    #prints whatever title you call between "[]"
+print(girlfriend_bio["fav snack"])
